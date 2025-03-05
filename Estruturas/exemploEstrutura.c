1d6821d280f3d72fc7b0ae196182733c86f4a98d
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-03-05 18:49:52 -0300

    acrescetando mais uma linha no código "

--- Estruturas/exemploEstrutura.c
+++ Estruturas/exemploEstrutura.c
@@ -2,11 +2,14 @@
 
 int main(){
     int numero1, numero2;
-    numero1 = 15;
-    numero2 = 10;
+    numero1 = 10;
+    numero2 = 15;
     if (numero1>numero2)
     {
-        printf("O numero 1 é menor que o numero 2");
+        printf("O numero 1 é maior que o numero 2");
+    } else {
+        
+        printf("O numero 2 é maior que o numero 1");
     }
     
 

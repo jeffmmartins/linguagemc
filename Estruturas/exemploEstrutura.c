cde97e94678940c3a8e86dc0e7f31d182dac290d
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-03-05 18:38:01 -0300

    exemplo de estrutura de decisão finalizado

---
+++ Estruturas/exemploEstrutura.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int numero1, numero2;
+    numero1 = 15;
+    numero2 = 10;
+    if (numero1>numero2)
+    {
+        printf("O numero 1 é menor que o numero 2");
+    }
+    
+
+}
